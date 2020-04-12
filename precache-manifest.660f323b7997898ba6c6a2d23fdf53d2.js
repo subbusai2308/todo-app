a3e3deff8b4ec8d3995d86d0c9253786d416b81d
Subramanian CR <43859138+subbusai2308@users.noreply.github.com>
2020-04-12 12:15:21 +0530

    Updates

---
+++ precache-manifest.660f323b7997898ba6c6a2d23fdf53d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b90fd2dc54cc225c5af95d961a2eab",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "debabe2c1cbe6cfe202f",
+    "url": "/todo-app/static/css/main.afba6f3a.chunk.css"
+  },
+  {
+    "revision": "a9f46057e6a8cf033ea8",
+    "url": "/todo-app/static/js/2.6e0535fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.6e0535fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debabe2c1cbe6cfe202f",
+    "url": "/todo-app/static/js/main.ed44c78e.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  }
+]);
